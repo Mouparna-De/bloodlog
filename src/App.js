4b0f6d7e8132c5d9f9ff0a2cd357491dbf736445
siddhant-sid-s <siddhantshaw1@gmail.com>
2024-04-19 11:17:43 +0530

    full

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import FAQ from './Components/FAQ/Faq';
+import Navbarcom from './Components/nav/Navbarcom';
+import Home from './Components/pages/Home';
+import About from './Components/pages/About';
+import Contact from './Components/pages/Contact';
+import Footer from './Components/footer/Footer';
+import Register from './Components/pages/Register';
+import Login from './Components/pages/Login';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbarcom/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Contact" element={<Contact/>} />
+          <Route path="/FAQ" element={<FAQ/>} />
+          <Route path="/Register" element={<Register/>} />
+          <Route path="/Login" element={<Login/>} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
   );
 }
 
